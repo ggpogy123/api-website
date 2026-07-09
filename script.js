2ea54ddd2c9e069100418f0712649bbe74b4c85a
ggpogy123 <singhaadish26@gmail.com>
2026-07-09 20:40:06 +0530

    time logic done

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ async function getbackground() {
 }
 
 window.onload=function(){
+    clk();
     getbackground().then(function (imageUrl){
         if(!imageUrl) return;
         console.log(imageUrl);
@@ -38,3 +39,29 @@ window.onload=function(){
         }
     });
 };
+
+function clk(){
+    const timeElem=document.getElementById("time");
+    if(!timeElem) return;
+
+    setInterval(()=>{
+        const dateObj=new Date();
+        let ms=dateObj.getTime();
+        const offsetms=dateObj.getTimezoneOffset()*60*1000;
+        let localms=ms-offsetms;
+        const dayMs=24*60*60*1000;
+        let todayMs= localms% dayMs;
+        let hrs=Math.floor(todayMs/(60*60*1000));
+        let mins=Math.floor(todayMs%(60*60*1000)/(60*1000));
+        let secs=Math.floor((todayMs%(60*1000))/1000);
+
+        
+        
+        //adding 0 before single digit time(formatting)
+        let fHrs=String(hrs).padStart(2,'0');
+        let fMins=String(mins).padStart(2,'0');
+        let fSecs=String(secs).padStart(2,'0');
+
+        timeElem.innerText=`${fHrs}:${fMins}:${fSecs}`;
+    },1000);
+}
